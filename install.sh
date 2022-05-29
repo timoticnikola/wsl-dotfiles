831f61f16066ee19523f8e9b7310b51618f9b107
Nikola Timotic <timoticnikola.01@gmail.com>
2022-05-29 03:52:28 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install vim zsh curl wget -y
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+cp ~/.zshrc ~/.zshrc.bk
+cp .zshrc ~/.zshrc
+
+zsh
+
+source ~/.zshrc
+
